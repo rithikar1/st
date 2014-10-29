0acfc30f3e8d33f2d4d6e5f000a90b5191adb6c2
kwaghray <kunalwaghray52@gmail.com>
2014-10-30 00:43:10 +0530

    this is my first commit to github

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,8 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+  def aboutus
+  end
+  def contactus
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stationary::Application.config.secret_token = 'e5c92c447cef780ffaef548fc2565dec3a3551f362067e0e6ca272cb8e1c4fc23870f5bb48402cb5c0e3af2ee796a00dc6c313a52dc77412e00d238091a1caba'
